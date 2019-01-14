e77c7f4554d583809c3bd3e102ee454c4df84d5b
jashment <jordanashment@gmail.com>
2019-01-14 15:02:57 -0700

    Small <p> addition

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const server = http.createServer((request, response) => {
     response.setHeader('Content-Type', 'text/html')
     response.write('<html>')
     response.write('<head><title>Testing my Server</title></head>')
-    response.write('<body><h1>Welcome to my server.</h1></body>')
+    response.write('<body><h1>Welcome to my server.</h1><p>Wanting to see if this will auto-update.</p></body>')
     response.write('</html>')
     return response.end()
 })
