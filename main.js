555298417ff6291035a4657e4798a32e818392e7
jashment <jordanashment@gmail.com>
2019-01-16 14:43:35 -0700

    various changes/added routes.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const http = require('http')
-const server = http.createServer((request, response) => {
-    response.setHeader('Content-Type', 'text/html')
-    response.write('<html>')
-    response.write('<head><title>Testing my Server</title></head>')
-    response.write('<body><h1>Welcome to my server.</h1><p>Wanting to see if this will auto-update.</p></body>')
-    response.write('</html>')
-    return response.end()
-})
+
+const routes = require('./routes')
+
+console.log(routes.someText)
+
+const server = http.createServer(routes.handler)
 
 server.listen(3000)
