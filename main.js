37014be1647000ffc3575a37ebab10f4e848feb1
jashment <jordanashment@gmail.com>
2019-01-27 15:14:05 -0700

    added page-not-found.html and routes

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const bodyParser = require('body-parser')
 
+const path = require('path')
+
 const express = require('express')
 
 const app = express()
@@ -14,7 +16,7 @@ app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'))
 })
 
 app.listen(3000)
