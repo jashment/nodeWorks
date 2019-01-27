f49b99d4ee2ed56ea8344bf257c92566881d34ac
jashment <jordanashment@gmail.com>
2019-01-27 14:51:32 -0700

    adding admin and shop with routes

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const http = require('http')
+const bodyParser = require('body-parser')
 
 const express = require('express')
 
 const app = express()
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+
 app.use((req, res, next) => {
-    console.log('In the other middleware')
-    res.send('<h1>Hello from Express</h1>')
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 
 app.listen(3000)
