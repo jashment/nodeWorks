c257253fd18c4bda41b099921dbd48d4fe0d47cd
jashment <jordanashment@gmail.com>
2019-01-27 17:56:36 -0700

    Working with EJS

--- main.js
+++ main.js
@@ -7,17 +7,20 @@ const express = require('express')
 
 const app = express()
 
-const adminRoutes = require('./routes/admin')
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'))
+    res.status(404).render('page-not-found', { pageTitle: 'Page Not Found'})
 })
 
 app.listen(3000)
