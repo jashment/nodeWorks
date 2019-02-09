66b34e7b20b578ddac9fa230ca82611a0d3dff36
jashment <jordanashment@gmail.com>
2019-02-09 15:40:25 -0700

    Working with ejs

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,27 @@
+const path = require('path')
+
 const express = require('express')
 
 const router = express.Router()
 
+const products = []
+
+const rootDir = require('../util/path')
+
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>')
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProducts: true
+    })
 })
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body)
+    products.push({title: req.body.title})
     res.redirect('/')
 })
 
-module.exports = router
+exports.routes = router
+exports.products = products
