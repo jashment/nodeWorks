f92780763980e48912a283812fafe1f3b4c63a14
jashment <jordanashment@gmail.com>
2019-02-09 12:17:29 -0700

    Basically starting over

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
-const http = require('http')
-
-const path = require('path')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const mongoConnect = require('./utilities/database').mongoConnect
-
 const app = express()
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-
-//const adminRoutes = require('./routes/admin')
-//const shopRoutes = require('./routes/shop')
-
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
 
-
-
-//app.use('/admin', adminData.routes)
-//app.use(shopRoutes)
-
-app.use((req, res, next) => {
-    res.status(404).render('page-not-found', { pageTitle: 'Page Not Found'})
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>')
 })
 
-mongoConnect(() => {
-    app.listen(3000)
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
 })
 
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello!</h1>')
+})
+
+app.listen(3000)
