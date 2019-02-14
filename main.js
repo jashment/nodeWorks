f8e9ff44b4025b73350bd296f3d029e1b1192df2
jashment <jordanashment@gmail.com>
2019-02-13 19:31:46 -0700

    Creating user and connections

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 
 const errorController = require('./controllers/error')
 const mongoConnect = require('./util/database').mongoConnect
+const User = require('./models/user')
 
 const app = express()
 
@@ -17,12 +18,19 @@ app.set('views', 'views')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+    User.findById('5c64d295129400c6ec807a84')
+    .then(user => {
+        req.user = user
+        next()
+    })
+})
+
  app.use('/admin', adminRoutes)
  app.use(shopRoutes)
 
 app.use(errorController.get404)
 
 mongoConnect(() => {
-    console.log()
     app.listen(3000)
 })
