9c6b067eb0d73836045019bc8ed0eb0940c1876b
jashment <jordanashment@gmail.com>
2019-02-13 19:14:37 -0700

    Fixxing routes and various small details

--- main.js
+++ main.js
@@ -3,22 +3,26 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const errorController = require('./controllers/404')
+const errorController = require('./controllers/error')
+const mongoConnect = require('./util/database').mongoConnect
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+ const adminRoutes = require('./routes/admin')
+ const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
+ app.use('/admin', adminRoutes)
+ app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-app.listen(3000)
+mongoConnect(() => {
+    console.log()
+    app.listen(3000)
+})
